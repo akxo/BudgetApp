0d6d179410706b04a9f149807209e9a0e77bbfd6
Alexander Kerendian <atrix113@aol.com>
2018-10-01 18:32:10 -0400

    changes name of custom categories

--- BudgetApp/Controller/AddCategoryViewController.swift
+++ BudgetApp/Controller/AddCategoryViewController.swift
@@ -14,7 +14,7 @@ class AddCategoryViewController: UIViewController, UITableViewDelegate, UITableV
     
     var filteredSuggestedCategories: [String] = []
     
-    var filteredCustomCategories: [Category] = []
+    var customCategories: [Category] = []
     
     var isSearching: Bool = false
     
@@ -40,7 +40,7 @@ class AddCategoryViewController: UIViewController, UITableViewDelegate, UITableV
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        filteredCustomCategories = OverviewViewController.budget.categories
+        customCategories = OverviewViewController.budget.categories
         categoriesTableView.reloadData()
     }
     
@@ -59,11 +59,11 @@ class AddCategoryViewController: UIViewController, UITableViewDelegate, UITableV
         if searchText != "" {
             let text = searchText.lowercased()
             filteredSuggestedCategories = suggestedCategories.filter({ $0.lowercased().contains(text) })
-            filteredCustomCategories = OverviewViewController.budget.categories.filter({ $0.name.lowercased().contains(text) })
+            customCategories = OverviewViewController.budget.categories.filter({ $0.name.lowercased().contains(text) })
             isSearching = true
         } else {
             filteredSuggestedCategories = suggestedCategories
-            filteredCustomCategories = OverviewViewController.budget.categories
+            customCategories = OverviewViewController.budget.categories
             isSearching = false
         }
         categoriesTableView.reloadData()
@@ -84,13 +84,13 @@ class AddCategoryViewController: UIViewController, UITableViewDelegate, UITableV
             if section == 0 {
                 return nil
             } else if section == 1 {
-                return filteredCustomCategories.count > 0 ? "MY CATEGORIES" : nil
+                return customCategories.count > 0 ? "MY CATEGORIES" : nil
             } else if section == 2 {
                 return "SUGGESTED CATEGORIES"
             }
         } else {
             if section == 0 {
-                return filteredCustomCategories.count > 0 ? "MY CATEGORIES" : nil
+                return customCategories.count > 0 ? "MY CATEGORIES" : nil
             } else if section == 1 {
                 return "SUGGESTED CATEGORIES"
             }
@@ -109,13 +109,13 @@ class AddCategoryViewController: UIViewController, UITableViewDelegate, UITableV
             if section == 0 {
                 return 1
             } else if section == 1 {
-                return filteredCustomCategories.count
+                return customCategories.count
             } else if section == 2 {
                 return filteredSuggestedCategories.count
             }
         } else {
             if section == 0 {
-                return filteredCustomCategories.count
+                return customCategories.count
             } else if section == 1 {
                 return filteredSuggestedCategories.count
             }
@@ -128,19 +128,23 @@ class AddCategoryViewController: UIViewController, UITableViewDelegate, UITableV
         let cell = categoriesTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         if isSearching {
             if section == 0 {
-                cell.textLabel?.text = "Create \"\(categorySearchBar.text!)\""
+                if let cat = customCategory(for: categorySearchBar.text ?? "") {
+                    cell.textLabel?.text = "Edit \"\(cat)\""
+                } else {
+                    cell.textLabel?.text = "Create \"\(categorySearchBar.text!)\""
+                }
                 cell.detailTextLabel?.text = ""
             } else if section == 1 {
-                cell.textLabel?.text = filteredCustomCategories[indexPath.row].name
-                cell.detailTextLabel?.text = "$\(filteredCustomCategories[indexPath.row].limit)"
+                cell.textLabel?.text = customCategories[indexPath.row].name
+                cell.detailTextLabel?.text = "$\(customCategories[indexPath.row].limit)"
             } else if section == 2 {
                 cell.textLabel?.text = filteredSuggestedCategories[indexPath.row]
                 cell.detailTextLabel?.text = ""
             }
         } else {
             if section == 0 {
-                cell.textLabel?.text = filteredCustomCategories[indexPath.row].name
-                cell.detailTextLabel?.text = "$\(filteredCustomCategories[indexPath.row].limit)"
+                cell.textLabel?.text = customCategories[indexPath.row].name
+                cell.detailTextLabel?.text = "$\(customCategories[indexPath.row].limit)"
             } else if section == 1 {
                 cell.textLabel?.text = filteredSuggestedCategories[indexPath.row]
                 cell.detailTextLabel?.text = ""
@@ -176,5 +180,10 @@ class AddCategoryViewController: UIViewController, UITableViewDelegate, UITableV
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    private func customCategory(for name: String) -> String? {
+        return customCategories.first(where: { $0.name.lowercased() == name.lowercased() })?.name
+    }
+    
 }
 
